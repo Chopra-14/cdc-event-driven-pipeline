2a3a697e4161085617a681c6cc8d48f6fb9e0cd9
teddy <sathvikakonakalla14@gmail.com>
2026-02-09 17:27:31 +0530

    Set Kafka consumer to read from earliest offset

--- kafka-consumer/consumer.py
+++ kafka-consumer/consumer.py
@@ -5,6 +5,8 @@ import os
 consumer = KafkaConsumer(
     os.getenv("KAFKA_TOPIC"),
     bootstrap_servers=os.getenv("KAFKA_BOOTSTRAP_SERVERS"),
+    auto_offset_reset='earliest',
+    enable_auto_commit=True,
     value_deserializer=lambda m: json.loads(m.decode("utf-8"))
 )
 
